b559651fdfb37d4674f0f6859b124c665021004c
Aneta Damaszke <aneta.damaszke.programista@gmail.com>
2022-10-09 20:05:29 +0200

    Downloading user categories to the drop-down menu

--- income.php
+++ income.php
@@ -5,46 +5,54 @@
     if(!isset($_SESSION['logged_on'])) {
         header('Location: login.php');
         exit();
-    }
+    } else {
 
-    require_once "database.php";
+        require_once "database.php";
 
-    $userId = $_SESSION['userId'];
+        $userId = $_SESSION['userId'];
 
-    // ladowanie kategorii użytkownika:
-    $getCategoryName=$db->query("SELECT category_name 
-    FROM user_incomes_categories 
-    WHERE user_id='$userId'");
-    $categoryName = $getCategoryName->fetch();
-                                                
+        // ładowanie kategorii użytkownika:
+        $getCategoryName=$db->query("SELECT category_name 
+        FROM user_incomes_categories 
+        WHERE user_id='$userId'");                                             
 
-    // dodawanie przychodu do bazy:
-    if(isset($_POST['incomeValue'])) {
-        
-        $value = $_POST['incomeValue'];
-        $date = $_POST['incomeDate'];
-        $comment = $_POST['comment'];
-        $category = $_POST['category'];
+        // dodawanie przychodu do bazy:
+        if(isset($_POST['incomeValue'])) {
+            
+            $value = $_POST['incomeValue'];
+            $date = $_POST['incomeDate'];
+            $comment = $_POST['comment'];
+            $category = $_POST['category'];
+
+            // pobierania ID kategorii przychodu:
+            $getIncomeId=$db->query("SELECT id FROM user_incomes_categories WHERE category_name='$category'");
+            $categoryId = $getIncomeId->fetchColumn();
+            
+            $query = $db->prepare('INSERT INTO user_incomes VALUES (NULL, :userid, :value, :date, :categoryid, :comment)');
+            $query -> bindValue(':userid', $userId, PDO::PARAM_INT);
+            $query -> bindValue(':value', $value, PDO::PARAM_STR);
+            $query -> bindValue(':date', $date, PDO::PARAM_STR);
+            $query -> bindValue(':categoryid', $categoryId, PDO::PARAM_INT);
+            $query -> bindValue(':comment', $comment, PDO::PARAM_STR);
+            $query -> execute();
+
+            $is_ok = true;
+            $_SESSION['comment'] = 'Świetnie! Dodano nowy przychód do bazy!';
+        } else {
+            $is_ok = false;
+            $_SESSION['comment'] = 'Błąd!';
+        }
 
         // dodawanie nowej kategorii:
-        $newCategory = $_POST['addNewIncomeCat'];
-        $addNewIncomeCat = $db->prepare('INSERT INTO user_incomes_categories 
-        VALUES (NULL, :userid, :newCategoryName)');
-        $addNewIncomeCat -> bindValue(':userid', $userId, PDO::PARAM_INT);
-        $addNewIncomeCat -> bindValue(':newCategoryName', $newCategory, PDO::PARAM_STR);
-        $addNewIncomeCat -> execute();
+        if(isset($_POST['addNewIncomeCat'])) {
 
-        // pobierania ID kategorii przychodu:
-        $getIncomeId=$db->query("SELECT id FROM user_incomes_categories WHERE category_name='$category'");
-        $categoryId = $getIncomeId->fetchColumn();
-        
-        $query = $db->prepare('INSERT INTO user_incomes VALUES (NULL, :userid, :value, :date, :categoryid, :comment)');
-        $query -> bindValue(':userid', $userId, PDO::PARAM_INT);
-        $query -> bindValue(':value', $value, PDO::PARAM_STR);
-        $query -> bindValue(':date', $date, PDO::PARAM_STR);
-        $query -> bindValue(':categoryid', $categoryId, PDO::PARAM_INT);
-        $query -> bindValue(':comment', $comment, PDO::PARAM_STR);
-        $query -> execute();
+            $newCategory = $_POST['addNewIncomeCat'];
+            $addNewIncomeCat = $db->prepare('INSERT INTO user_incomes_categories 
+            VALUES (NULL, :userid, :newCategoryName)');
+            $addNewIncomeCat -> bindValue(':userid', $userId, PDO::PARAM_INT);
+            $addNewIncomeCat -> bindValue(':newCategoryName', $newCategory, PDO::PARAM_STR);
+            $addNewIncomeCat -> execute();
+        }
     }
 
 ?>
@@ -65,7 +73,7 @@
         <!-- Navbar -->
         <nav class="nav navbar navbar-light navbar-expand-md menu">
             <div class="container">
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="index.php">
                     <img class="menu-logo" src="img/MYBUDGET.svg" alt="logo" />
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navContent">
@@ -73,10 +81,10 @@
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navContent">
                     <div class="navbar-nav">
-                        <a href="expense.html" class="nav-item nav-link menu-link">Dodaj wydatek</a>
-                        <a href="balance.html" class="nav-item nav-link menu-link">Zobacz bilans</a>
-                        <a href="setting.html" class="nav-item nav-link ms-2 ms-lg-3 menu-link">Ustawienia</a>
-                        <a href="index.html" class="nav-item nav-link ms-2 ms-lg-3 menu-link">Wyloguj</a>
+                        <a href="expense.php" class="nav-item nav-link menu-link">Dodaj wydatek</a>
+                        <a href="balance.php" class="nav-item nav-link menu-link">Zobacz bilans</a>
+                        <a href="setting.php" class="nav-item nav-link ms-2 ms-lg-3 menu-link">Ustawienia</a>
+                        <a href="logout.php" class="nav-item nav-link ms-2 ms-lg-3 menu-link">Wyloguj</a>
                     </div>
                 </div>
             </div>
@@ -110,33 +118,55 @@
                                             <div id="categoryCheck" class="form-check">
                                                 <label class="form-check-label d-block mb-2">Kategoria:</label>
                                                 <select name="category" class="me-3">
-                                                    <?php                                                
-                                                        foreach ($categoryName as $category) {
-                                                           echo "<option>{$category}</option>";
+                                                    <option selected="selected">Wybierz:</option>
+                                                    <?php 
+                                                        while ($name = $getCategoryName ->fetch())
+                                                        {
+                                                            echo '<option>'.$name['category_name'].'</option>';
                                                         }
                                                     ?>
                                                 </select>
                                             </div>
                                         </div>
-                                        
+                                        <button type="button" class="btn form-sm-button" data-bs-toggle="modal" data-bs-target="#addCatModal">
+                                        Dodaj nową
+                                        </button>
                                     </div>
-                                    <div class="col-md-6">
-                                            <div id="categoryCheck" class="form-check">
-                                                <label class="form-check-label d-block mb-2"></label>
-                                                <div class="form-group pt-4">
-                                                    <label for="addNewIncomeCat" class="form-control-label d-block mb-2">Nazwa nowej kategorii:</label>
-                                                    <input name="addNewIncomeCat" class="form-control d-block w-100 p-3" />
-                                                    <button type="submit" class="btn form-sm-button">Dodaj</button>
-                                                </div> 
+
+                                    <!-- Modal z dodawaniem nowej kategorii -->
+                                    <div class="modal fade" id="addCatModal" role="dialog" tabindex="-1" aria-labelledby="addCatModall" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="addCatModall">Dodaj nową kategorię przychodu</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="col-md-6">
+                                                <div id="categoryCheck" class="form-check">
+                                                    <label class="form-check-label d-block mb-2"></label>
+                                                    <div class="form-group pt-4">
+                                                        <label for="addNewIncomeCat" class="form-control-label d-block mb-2">Nazwa nowej kategorii:</label>
+                                                        <input name="addNewIncomeCat" class="form-control d-block w-100 p-3" />                                
+                                                    </div> 
+                                                </div>
                                             </div>
                                         </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Anuluj</button>
+                                            <button type="submit" class="btn btn-primary">Dodaj do bazy</button>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    
                                     <div class="form-group pt-4">
                                         <label for="comment" class="form-control-label d-block mb-2">Komentarz (opcjonalnie)</label>
                                         <input name="comment" class="form-control d-block w-100 p-3" />
                                     </div>
                                 </div>
                                 <div class="text-center statement"></div>
-                                <div class="row pt-2">
+                                <div class="row pt-2 pb-2">
                                     <div class="col-md-6">
                                         <a href="menu.html" type="button" class="btn form-button__secondary">Anuluj</a>
                                     </div>
@@ -144,6 +174,13 @@
                                         <button id="addIncomeBtn" type="submit" class="btn form-button">Dodaj</button>
                                     </div>
                                 </div>
+                                <?php 
+                                    if($is_ok == true)
+                                    {
+                                        echo '<div>'.$_SESSION['comment'].'</div>';
+                                        unset($_SESSION['comment']);
+                                    }
+                                ?>
                             </form>
                         </div>
                     </div>
